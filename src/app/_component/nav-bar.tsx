065324dd32ccf7eb2abd43a01f287b0c5b0773ba
reiym1 <subaru4791@gmail.com>
2023-11-21 21:47:12 +0900

    chg

---
+++ src/app/_component/nav-bar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <header className="bg-gray-800 p-4">
+      <nav className="space-x-4">
+        <Link
+          href="/"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          Home
+        </Link>
+        <Link
+          href="blogs"
+          className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+        >
+          {/* <Login /> */}
+        </Link>
+      </nav>
+    </header>
+  )
+}
