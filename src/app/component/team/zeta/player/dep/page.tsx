312a748afb9c1599d7d539f8065424f4386779d1
reiym1 <subaru4791@gmail.com>
2023-12-05 21:48:37 +0900

    chg

--- src/app/component/team/zeta/player/dep/page.tsx
+++ src/app/component/team/zeta/player/dep/page.tsx
@@ -12,7 +12,12 @@ export default function DepPage() {
                     height={300}
         />
     </div>
-    <div></div>
+    <div>
+        <h2>実績</h2>
+        <p>ここに今までの実績</p>
+    </div>
+    <h3>選手紹介</h3>
+    <p>ここに選手紹介文</p>
     <div>
         <h4>使用デバイス</h4>
             <p>マウス: Razer Viper V2 PRO WHITE</p>   
