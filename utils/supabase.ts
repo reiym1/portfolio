ab7a664a712f92deb5fec893d44859f190f48413
reiym1 <subaru4791@gmail.com>
2023-12-08 17:24:57 +0900

    chg

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 import { Database } from '../database.types'
-import { createClient } from '@supabase/supabase-js'
 
-export default createClient<Database>(
+export default createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
