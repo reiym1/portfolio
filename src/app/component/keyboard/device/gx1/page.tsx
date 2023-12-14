04f5861798dd83c0f0acad4e2b879270388c3c65
reiym1 <subaru4791@gmail.com>
2023-12-14 16:13:34 +0900

    変更

--- src/app/component/keyboard/device/gx1/page.tsx
+++ src/app/component/keyboard/device/gx1/page.tsx
@@ -2,43 +2,27 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function KeyboardPage() {
-    return (
-        <main>
-            <div className="text-2xl">
-            <h1>企業一覧</h1>
-            </div>
-        <div className="flex justify-center">
-            <Image
-                src="/icon-image/realforce.png"
-                alt="会社ロゴ"
-                width={200}
-                height={300}
-            />
+  return (
+    <main>
+      <div className="text-center text-2xl">
+        <h1 className="">GX1 KEYBOARD / X1UD13</h1>
+      </div>
+      <div className="flex justify-center m-4">
+        <div className="m-4">
+          <Image
+            src="/device-image/gx1.jpg"
+            alt="gx1画像"
+            width={300}
+            height={400}
+          />
+          <p className="text-center text-2xl"></p>
         </div>
-        <div className="flex m-4">
-            <div className="m-4">
-            <Link href="/component/keyboard/device/gx1">
-            <Image
-                src="/device-image/gx1.jpg"
-                alt="gx1画像"
-                width={300}
-                height={400}
-            />
-            </Link>
-            <p className="text-2xl">GX1 KEYBOARD / X1UD13</p>
-            </div>
-            <div className="m-4">
-            <Link href="/component/keyboard/device/gx111">
-            <Image
-                src="/device-image/gx111.jpg"
-                alt="gx1画像"
-                width={300}
-                height={400}
-            />
-            </Link>
-            <p className="text-2xl">GX1 KEYBOARD / X1UD11</p>
-            </div>
-        </div>
-        </main>
-    )
-    }
+      </div>
+      <div>
+        <Link href="https://www.realforce.co.jp/products/X1UD13/">
+          <h2>こちらから公式ページに飛べます！</h2>
+        </Link>
+      </div>
+    </main>
+  )
+}
