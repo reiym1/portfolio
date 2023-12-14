c530545d5006c611e9929948882a970803cbbb35
reiym1 <subaru4791@gmail.com>
2023-12-14 21:08:18 +0900

    chg

--- src/app/component/keyboard/device/gx1/page.tsx
+++ src/app/component/keyboard/device/gx1/page.tsx
@@ -1,28 +1,37 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import DeviceReviewPage from '@/app/device_reviews/page'
 
 export default function KeyboardPage() {
-  return (
+    const text = "REALFORCEのキーボードは静電容量無接点方式が採用されており、非常にタイピングがしやすく耐久性も高い\n打鍵感はあまりなくしっかり押したい方にはあまりお勧めではない\n余談だがどうやらふるさと納税でこのキーボードを返礼品として頂ける事には驚いた";
+
+return (
     <main>
-      <div className="text-center text-2xl">
+    <div className="text-center text-2xl">
         <h1 className="">GX1 KEYBOARD / X1UD13</h1>
-      </div>
-      <div className="flex justify-center m-4">
+    </div>
+    <div className="flex justify-center m-4">
         <div className="m-4">
-          <Image
+        <Image
             src="/device-image/gx1.jpg"
             alt="gx1画像"
             width={300}
             height={400}
-          />
-          <p className="text-center text-2xl"></p>
+        />
+    <div className="flex flex-col items-center">
+    <p className="whitespace-pre-wrap">{text}</p>
+    </div>
         </div>
-      </div>
-      <div>
+    </div>
+    <div className="text-center text-2xl">
         <Link href="https://www.realforce.co.jp/products/X1UD13/">
-          <h2>こちらから公式ページに飛べます！</h2>
+        <h2>こちらから公式ページに飛べます！</h2>
         </Link>
-      </div>
+    </div>
+    <div className="text-center text-1xl mt-5">
+        <h3>デバイスレビュー</h3>
+        <DeviceReviewPage />
+    </div>
     </main>
-  )
+)
 }
